104d82980c645c0c2e958b29823004f565c10ee2
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2021-06-24 20:08:13 +0100

    Finally add bottom sheet

--- app/src/main/java/com/pppp/todo/main/todo/MainScreen.kt
+++ app/src/main/java/com/pppp/todo/main/todo/MainScreen.kt
@@ -4,24 +4,55 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pppp.todo.exaustive
 import com.pppp.todo.main.MainViewModel
 import com.pppp.todo.main.TodoMainViewModel
+import kotlinx.coroutines.launch
+import androidx.compose.material.ModalBottomSheetValue.Hidden
+import kotlinx.coroutines.CoroutineScope
 
 @ExperimentalMaterialApi
 @Composable
 fun MainScreen(mainViewModel: MainViewModel) {
     val state: TodoMainViewModel by mainViewModel.uiState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
-    Scaffold(
-        floatingActionButton = { Fab {} }
-    ) {
-        BottomSheetLayout(state)
+    val modalBottomSheetState = rememberModalBottomSheetState(Hidden)
+    Box {
+        Scaffold(
+            floatingActionButton = {
+                Fab {
+                    toggleBottomSheet(coroutineScope, modalBottomSheetState)
+                }
+            },
+            isFloatingActionButtonDocked = true,
+            bottomBar = {
+                BottomAppBar {}
+            }
+        ) {
+            Content(state)
+        }
+        BottomSheetLayout(modalBottomSheetState)
+    }
+}
+
+@ExperimentalMaterialApi
+private fun toggleBottomSheet(
+    coroutineScope: CoroutineScope,
+    modalBottomSheetState: ModalBottomSheetState
+) {
+    coroutineScope.launch {
+        if (modalBottomSheetState.isVisible) {
+            modalBottomSheetState.hide()
+        } else {
+            modalBottomSheetState.show()
+        }
     }
 }
 
@@ -40,18 +71,25 @@ fun Fab(onClick: () -> Unit) {
 
 @ExperimentalMaterialApi
 @Composable
-private fun BottomSheetLayout(state: TodoMainViewModel) {
-    val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded)
+private fun BottomSheetLayout(
+    modalBottomSheetState: ModalBottomSheetState
+) {
+
     ModalBottomSheetLayout(
-        sheetState = sheetState,
-        sheetContent = { Text(text = "Foo") },
-        content = {
-            when (state.isLoading) {
-                true -> ListOfToDos(state)
-                else -> Loading()
-            }.exaustive
-        }
+        sheetState = modalBottomSheetState,
+        sheetContent = {
+            AddToDo()
+        },
+        content = {}
     )
+}
+
+@Composable
+private fun Content(state: TodoMainViewModel) {
+    when (state.isLoading) {
+        true -> ListOfToDos(state)
+        else -> Loading()
+    }.exaustive
 }
 
 @Composable
