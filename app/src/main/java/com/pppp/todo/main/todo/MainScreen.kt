c46e85f2fff3c4a91e5571f05ca3f0805df56252
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2021-06-24 19:21:33 +0100

    Try and add bottom sheet

--- app/src/main/java/com/pppp/todo/main/todo/MainScreen.kt
+++ app/src/main/java/com/pppp/todo/main/todo/MainScreen.kt
@@ -2,58 +2,27 @@ package com.pppp.todo.main.todo
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.BottomSheetValue.Collapsed
-import androidx.compose.material.BottomSheetValue.Expanded
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pppp.todo.exaustive
 import com.pppp.todo.main.MainViewModel
-import com.pppp.todo.main.ToDoViewEvent
 import com.pppp.todo.main.TodoMainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
-
+@ExperimentalMaterialApi
 @Composable
 fun MainScreen(mainViewModel: MainViewModel) {
     val state: TodoMainViewModel by mainViewModel.uiState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
     Scaffold(
-        scaffoldState = bottomSheetScaffoldState(state),
-        floatingActionButton = { Fab { onFabClickd(coroutineScope, mainViewModel) } },
-        sheetContent = { AddToDo() }
+        floatingActionButton = { Fab {} }
     ) {
-        ToDoScreen(state)
+        BottomSheetLayout(state)
     }
-}
-
-private fun onFabClickd(
-    coroutineScope: CoroutineScope,
-    model: MainViewModel
-) {
-    coroutineScope.launch {
-        model(ToDoViewEvent.OnFabClicked)
-    }
-}
-
-@ExperimentalMaterialApi
-@Composable
-private fun bottomSheetScaffoldState(state: TodoMainViewModel): BottomSheetScaffoldState {
-    val bottomSheetState = if (state.addTodoViewModel == null) {
-        BottomSheetState(Collapsed)
-    } else {
-        BottomSheetState(Expanded)
-    }
-    return rememberBottomSheetScaffoldState(
-        bottomSheetState = bottomSheetState
-    )
 }
 
 @Composable
@@ -69,12 +38,20 @@ fun Fab(onClick: () -> Unit) {
     )
 }
 
+@ExperimentalMaterialApi
 @Composable
-private fun ToDoScreen(state: TodoMainViewModel) {
-    when (state.isLoading) {
-        true -> ListOfToDos(state)
-        else -> Loading()
-    }.exaustive
+private fun BottomSheetLayout(state: TodoMainViewModel) {
+    val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Expanded)
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { Text(text = "Foo") },
+        content = {
+            when (state.isLoading) {
+                true -> ListOfToDos(state)
+                else -> Loading()
+            }.exaustive
+        }
+    )
 }
 
 @Composable
